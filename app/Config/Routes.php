ae5ee027b18891441c9073952259e1966fc02b7c
Mario Cardoso <xcme044@gmail.com>
2026-02-23 16:14:25 -0600

    aplicando la nueva estructura a Login.php - Controlador y Routes.php

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -7,7 +7,7 @@ use CodeIgniter\Router\RouteCollection;
  */
 $routes->get('/', 'Home::index');
 $routes->get('test', 'Test::index');
-$routes->get('/usuario/login', 'Usuario\Login::index', ['as' => '']);
+$routes->get('/login', 'Usuario\Login::index', ['as' => '']);
 $routes->get('/dashboard', 'Panel\Dashboard::index', ['as' => 'dashboard']);
 /*'test' Es la URL. Eso es lo que tú escribes en el navegador: http://localhost:8081/test
 
--- app/Controllers/Usuario/Login.php
+++ app/Controllers/Usuario/Login.php
@@ -7,8 +7,24 @@ use CodeIgniter\HTTP\ResponseInterface;
 
 class Login extends BaseController
 {
-    public function index(): string
+    private $view = 'usuario/login';
+
+    private function load_data(){
+        $data = array();
+        // Cargue información global o específica
+        $data['nombre_pagina'] = 'Login';
+        $data['titulo_pagina'] = 'Inicio de Sesión';
+        // Statement SQL u otras configuraciones
+        return $data;
+    } // end load_data
+
+    private function create_view($name_view = '', $content = array()){
+        return view($name_view, $content);
+    } // end create_view
+
+    public function index()
     {
-        return view('usuario/login');
-    }
+        return $this->create_view($this->view, $this->load_data());
+    } // end index
 }
+// end Login
