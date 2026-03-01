41cccd0d4a5ba3ee67a83091b318121f76af4ac3
Mario Cardoso <xcme044@gmail.com>
2026-03-01 10:02:47 -0600

    tarea realizada

--- app/Views/panel/dashboard.php
+++ app/Views/panel/dashboard.php
@@ -3,3 +3,29 @@
 <?=$this->Section("Contenido");?>
 Hola
 <?=$this->endSection();?>
+
+<?= $this->section('css_especificos') ?>
+  <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+  <link rel="stylesheet" href="<?= base_url('recursos_panel/plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css') ?>">
+  <link rel="stylesheet" href="<?= base_url('recursos_panel/plugins/icheck-bootstrap/icheck-bootstrap.min.css') ?>">
+  <link rel="stylesheet" href="<?= base_url('recursos_panel/plugins/jqvmap/jqvmap.min.css') ?>">
+  <link rel="stylesheet" href="<?= base_url('recursos_panel/plugins/daterangepicker/daterangepicker.css') ?>">
+  <link rel="stylesheet" href="<?= base_url('recursos_panel/plugins/summernote/summernote-bs4.min.css') ?>">
+  <link rel="stylesheet" href="<?= base_url('recursos_panel/plugins/overlayScrollbars/css/OverlayScrollbars.min.css') ?>">
+<?= $this->endSection() ?>
+
+
+<?= $this->section('js_especificos') ?>
+  <script src="<?= base_url('recursos_panel/plugins/jquery-ui/jquery-ui.min.js') ?>"></script>
+  <script src="<?= base_url('recursos_panel/plugins/chart.js/Chart.min.js') ?>"></script>
+  <script src="<?= base_url('recursos_panel/plugins/sparklines/sparkline.js') ?>"></script>
+  <script src="<?= base_url('recursos_panel/plugins/jqvmap/jquery.vmap.min.js') ?>"></script>
+  <script src="<?= base_url('recursos_panel/plugins/jqvmap/maps/jquery.vmap.usa.js') ?>"></script>
+  <script src="<?= base_url('recursos_panel/plugins/jquery-knob/jquery.knob.min.js') ?>"></script>
+  <script src="<?= base_url('recursos_panel/plugins/moment/moment.min.js') ?>"></script>
+  <script src="<?= base_url('recursos_panel/plugins/daterangepicker/daterangepicker.js') ?>"></script>
+  <script src="<?= base_url('recursos_panel/plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js') ?>"></script>
+  <script src="<?= base_url('recursos_panel/plugins/summernote/summernote-bs4.min.js') ?>"></script>
+  <script src="<?= base_url('recursos_panel/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js') ?>"></script>
+  <script src="<?= base_url('recursos_panel/js/pages/dashboard.js') ?>"></script>
+<?= $this->endSection() ?>
--- app/Views/plantillas/panel_base.php
+++ app/Views/plantillas/panel_base.php
@@ -10,21 +10,22 @@
   <!-- Font Awesome -->
   <link rel="stylesheet" href="<?=base_url(DIR_RECUERSOS_PANEL_PLUGINS.'')?>/fontawesome-free/css/all.min.css">
   <!-- Ionicons -->
-  <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+ 
   <!-- Tempusdominus Bootstrap 4 -->
-  <link rel="stylesheet" href="<?=base_url(DIR_RECUERSOS_PANEL_PLUGINS.'/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css')?>">
+  
   <!-- iCheck -->
-  <link rel="stylesheet" href="<?=base_url(DIR_RECUERSOS_PANEL_PLUGINS.'/icheck-bootstrap/icheck-bootstrap.min.css')?>">
+  
   <!-- JQVMap -->
-  <link rel="stylesheet" href="<?=base_url(DIR_RECUERSOS_PANEL_PLUGINS.'/jqvmap/jqvmap.min.css')?>">
+  
   <!-- Theme style -->
   <link rel="stylesheet" href="<?=base_url(DIR_RECUERSOS_PANEL_CSS.'/adminlte.min.css')?>">
   <!-- overlayScrollbars -->
-  <link rel="stylesheet" href="<?=base_url(DIR_RECUERSOS_PANEL_PLUGINS.'/overlayScrollbars/css/OverlayScrollbars.min.css')?>">
+  
   <!-- Daterange picker -->
-  <link rel="stylesheet" href="<?=base_url(DIR_RECUERSOS_PANEL_PLUGINS.'/daterangepicker/daterangepicker.css')?>">
+  
   <!-- summernote -->
-  <link rel="stylesheet" href="<?=base_url(DIR_RECUERSOS_PANEL_PLUGINS.'/summernote/summernote-bs4.min.css')?>">
+  
+  <?=$this->renderSection("css_especificos");?>
 </head>
 <body class="hold-transition sidebar-mini layout-fixed">
 <div class="wrapper">
@@ -893,7 +894,7 @@
 <!-- jQuery -->
 <script src="<?=base_url(DIR_RECUERSOS_PANEL_PLUGINS.'/jquery/jquery.min.js')?>"></script>
 <!-- jQuery UI 1.11.4 -->
-<script src="<?=base_url(DIR_RECUERSOS_PANEL_PLUGINS.'/jquery-ui/jquery-ui.min.js')?>"></script>
+
 <!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
 <script>
   $.widget.bridge('uibutton', $.ui.button)
@@ -901,28 +902,27 @@
 <!-- Bootstrap 4 -->
 <script src="<?=base_url(DIR_RECUERSOS_PANEL_PLUGINS.'/bootstrap/js/bootstrap.bundle.min.js')?>"></script>
 <!-- ChartJS -->
-<script src="<?=base_url(DIR_RECUERSOS_PANEL_PLUGINS.'/chart.js/Chart.min.js')?>"></script>
+
 <!-- Sparkline -->
-<script src="<?=base_url(DIR_RECUERSOS_PANEL_PLUGINS.'/sparklines/sparkline.js')?>"></script>
+
 <!-- JQVMap -->
-<script src="<?=base_url(DIR_RECUERSOS_PANEL_PLUGINS.'/jqvmap/jquery.vmap.min.js')?>"></script>
-<script src="<?=base_url(DIR_RECUERSOS_PANEL_PLUGINS.'/jqvmap/maps/jquery.vmap.usa.js')?>"></script>
+
 <!-- jQuery Knob Chart -->
-<script src="<?=base_url(DIR_RECUERSOS_PANEL_PLUGINS.'/jquery-knob/jquery.knob.min.js')?>"></script>
+
 <!-- daterangepicker -->
-<script src="<?=base_url(DIR_RECUERSOS_PANEL_PLUGINS.'/moment/moment.min.js')?>"></script>
-<script src="<?=base_url(DIR_RECUERSOS_PANEL_PLUGINS.'/daterangepicker/daterangepicker.js')?>"></script>
+
 <!-- Tempusdominus Bootstrap 4 -->
-<script src="<?=base_url(DIR_RECUERSOS_PANEL_PLUGINS.'/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js')?>"></script>
+
 <!-- Summernote -->
-<script src="<?=base_url(DIR_RECUERSOS_PANEL_PLUGINS.'/summernote/summernote-bs4.min.js')?>"></script>
+
 <!-- overlayScrollbars -->
-<script src="<?=base_url(DIR_RECUERSOS_PANEL_PLUGINS.'/overlayScrollbars/js/jquery.overlayScrollbars.min.js')?>"></script>
+
 <!-- AdminLTE App -->
 <script src="<?=base_url(DIR_RECUERSOS_PANEL_JS.'/adminlte.js')?>"></script>
 <!-- AdminLTE for demo purposes -->
-<script src="<?=base_url(DIR_RECUERSOS_PANEL_JS.'/demo.js')?>"></script>
+
 <!-- AdminLTE dashboard demo (This is only for demo purposes) -->
-<script src="<?=base_url(DIR_RECUERSOS_PANEL_JS.'/pages/dashboard.js')?>"></script>
+
+<?=$this->renderSection("js_especificos");?>
 </body>
 </html>
